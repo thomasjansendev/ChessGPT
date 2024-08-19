bd60fc1cc9315650ee680d8b0a1bb63c7a99dc29
thomasjansendev <thomasjansen.dev@gmail.com>
2024-08-19 12:59:14 +0200

    added moveset for king and knight

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ board_ref = [["a8","b8","c8","d8","e8","f8","g8","h8"],
 
 def main():
     board = init_board()
-    # calc_possible_moves(board,"a1")
-    result = None
-    while result is None:
-        try:
-            result = new_move(board)
-            board = result
-        except Exception as e:
-            print(e)
+    debug_move(board, "e1","e4")
+    calc_possible_moves(board,"e4")
+
+    # idx = get_index("h7")
+    # print(idx)
+    # str = get_pos_name(idx)
+    # print(str)
+    
+    # result = None
+    # while result is None:
+    #     try:
+    #         result = new_move(board)
+    #         board = result
+    #     except Exception as e:
+    #         print(e)
 
 
 def init_board():
@@ -56,13 +63,16 @@ def init_board():
     # print_board(board)        
     return board
 
-def get_index(pos: str):
+def get_index(pos_str: str):
     #TODO (later): cache indicies into a dict and call get_index during initialization 
     #to avoid searching for index each time
     for r in range(0,len(board_ref)):
-        if pos in board_ref[r]:
-            index = (r, board_ref[r].index(pos))
+        if pos_str in board_ref[r]:
+            index = (r, board_ref[r].index(pos_str))
             return index
+
+def get_pos_name(pos_idx: tuple):
+    return board_ref[pos_idx[0]][pos_idx[1]]
 
 def print_board(board):
     for row in board:
@@ -80,8 +90,9 @@ def new_move(board):
     elif old_pos_str == new_pos_str:
         raise Exception("Must move piece to a different square than starting square.")
 
-    #check if move is legal
-    is_move_legal(board,old_pos_str,new_pos_str)
+    #check if move is legal -> error is raised if not
+    if new_pos_str not in calc_possible_moves(board,old_pos_str): 
+        raise Exception(f"Illegal move. Possible moves for selected piece are {calc_possible_moves(board,old_pos_str)}")
 
     #update board & print new state
     old_pos_index = get_index(old_pos_str)
@@ -92,16 +103,8 @@ def new_move(board):
 
     return board
 
-def is_move_legal(board,old_pos_str,new_pos_str):
-    
-    if new_pos_str not in calc_possible_moves(board,old_pos_str): 
-        raise Exception("Illegal move")
-    
-    #TODO check if another piece is blocking the movement of that piece
-
-    return True
-
 def calc_possible_moves(board: list,position_str: str):
+    piece_idx = get_index(position_str)
     piece_rank = get_index(position_str)[0]
     piece_file = get_index(position_str)[1]
     piece_id = board[piece_rank][piece_file]
@@ -109,51 +112,61 @@ def calc_possible_moves(board: list,position_str: str):
     if piece_id == " ": raise Exception(f"No piece is available to move on {position_str}")
     
     #TODO calculate legal moves for piece type
-    possible_moves = []
+    #TODO check if another piece is blocking the movement of that piece
+    possible_moves_str = []
+    possible_moves_idx = []
     match piece_id:
-        case "Q":
-            #move diagonally, up and across
+        
+        case "Q": # can move in any direction => 8 DOF - :
             pass
-        case "K":
-            #move up by one
-            #move across by one
+
+
+        case "K": # can move to any adjacent square by 1 => 8 DOF - 1
+            king_moveset = [(-1,0),(-1,+1),(0,+1),(+1,+1),(+1,0),(+1,-1),(0,-1),(-1,-1)]
+            for move in king_moveset:
+                new_move = (piece_idx[0]+move[0],piece_idx[1]+move[1]) #add moveset to the piece's current position
+                if 0 <= new_move[0] <= 7 and 0 <= new_move[1] <= 7: #only accept positions that are within board boundaries
+                    possible_moves_str.append(get_pos_name(new_move))
+
+
+        case "N": # can jump in L shape => 8 DOF - N/A
+            knight_moveset = [(-2,+1),(-1,+2),(+1,+2),(+2,+1),(+2,-1),(+1,-2),(-1,-2),(-2,-1)]
+            for move in knight_moveset:
+                new_move = (piece_idx[0]+move[0],piece_idx[1]+move[1]) #add moveset to the piece's current position
+                if 0 <= new_move[0] <= 7 and 0 <= new_move[1] <= 7: #only accept positions that are within board boundaries
+                    possible_moves_str.append(get_pos_name(new_move))
+
+        case "B": # can move diagonally => 4 DOF - :
             pass
-        case "N":
-            #jump in an L shape
+
+
+        case "R": # can move horizontally and vertically => 4 DOF - :
+            possible_moves_str += [rank[piece_file] for rank in board_ref] # vertical movement
+            possible_moves_str += board_ref[piece_rank][:] # horizontal movement
+            possible_moves_str = list(dict.fromkeys(possible_moves_str)) # removes duplicate values
+            possible_moves_str.remove(position_str) # removes current piece position
             pass
-        case "B":
-            #move diagonally
-            pass
-        case "R":
-            #move up
-            possible_moves += [rank[piece_file] for rank in board_ref]
-            #move across
-            possible_moves += board_ref[piece_rank][:]
-            pass
-        case "p":
+
+
+        case "p": # 1.5 DOF - 2->1
             #move up one
             #move up-two if on starting position
             #move diagonally one if it is capturing a piece
             pass
+
+
         case _:
             raise Exception(f"Invalid piece_id")
     
-    possible_moves = list(dict.fromkeys(possible_moves)) #removes duplicate values
-    possible_moves.remove(position_str) #removes current piece position
     
-    # print(possible_moves)
-    return possible_moves
+    print(possible_moves_str)
+    return possible_moves_str
 
-class Board:
-    def __init__(self):
-        pass
-
-
-class Pawn:
-    def __init__(self,colour):
-        self.__init__ = colour
-        self.position = ""
-
+def debug_move(board,old_pos_str,new_pos_str):
+    old_pos_index = get_index(old_pos_str)
+    new_pos_index = get_index(new_pos_str)
+    board[new_pos_index[0]][new_pos_index[1]] = board[old_pos_index[0]][old_pos_index[1]]
+    board[old_pos_index[0]][old_pos_index[1]] = " "
 
 
 main()

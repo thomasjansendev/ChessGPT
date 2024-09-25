9a01a6199027867290f2579756897179b3af34c9
thomasjansendev <thomasjansen.dev@gmail.com>
2024-09-25 19:36:53 +0200

    chore(pygame): ad pygame boilerplate to main

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
+import pygame
 from src.constants import *
 from src.core import *
 from src.pieces import *
+from src.ui import *
 
 def main():
-    board = init_board()
-    # board = init_empty_board()
-    # piece = Queen(Color.WHITE)
-    # pos = (2,2)
-    # board[pos[0]][pos[1]] = piece
-    
-    # piece_pos = piece.get_position(board)
-    # piece_moves = piece.calc_possible_moves(board)
-    # board = mark_moves_on_board(board,piece_moves)
-    # print_board(board,mode="--clean")
-    # print(f"Position: {piece_pos}")
-    # print(f"Possible moves: {piece_moves}")
-    
+    #Pygame Initialization
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     running = True
-    current_player = Color.WHITE
-    while running:
-        print(f"{current_player.name} to move: ")
-        result = None
-        while result is None:
-            try:
-                result = new_move(board,current_player)
-                board = result
-                print_board(board)
-            except Exception as e:
-                print(e)
-        
-        if current_player == Color.WHITE: current_player = Color.BLACK
-        elif current_player == Color.BLACK: current_player = Color.WHITE
-        
+    dt = 0
     
-    # print_board(board,mode="--clean")
+    #Chess Initialization
+    board = init_board()
+    current_player = Color.WHITE
+    
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                running = False
+                
+        screen.fill("black")
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+    
+        
+
+def handle_move() -> None:
+    print(f"{current_player.name} to move: ")
+    result = None
+    while result is None:
+        try:
+            result = new_move(board,current_player)
+            board = result
+            print_board(board)
+        except Exception as e:
+            print(e)
+    if current_player == Color.WHITE: current_player = Color.BLACK
+    elif current_player == Color.BLACK: current_player = Color.WHITE
+
 
 if __name__ == "__main__":
     main()

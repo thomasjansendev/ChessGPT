81ccd1448744e3cfec325f2019313d45a869125d
thomasjansendev <thomasjansen.dev@gmail.com>
2024-10-11 08:00:26 +0200

    feat(prompt): add system prompt to get UCI protocol compliant responses from chatGPT

--- src/ai.py
+++ src/ai.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 from os import system, name
+from src.board import *
 
 # Get API key
-api_key_path = r"C:\MyData\tmp\openai_api_key.txt"
+# api_key_path = r"C:\MyData\tmp\openai_api_key.txt"
+api_key_path = "/Users/thomasjansen/Workspace/api_keys/openai.txt"
 with open(api_key_path, 'r', encoding="utf-8") as file:
     api_key = file.read()
     
@@ -14,9 +16,12 @@ client = OpenAI(
 )
 
 # Initialize system prompt -> used to parametrize the output from the model
+with open("./src/sys_prompt.txt", 'r', encoding="utf-8") as file:
+    system_prompt_content = file.read()
+
 system_prompt = {
     "role": "system",
-    "content": "You are a chess grandmaster playing black, and your goal is to win as quickly as possible. I will provide the current game score before each of your moves, and your reply should just be your next move in algebraic notation with no other commentary. The desired output should follow this format: '<your_move>' and should NOT ressemble this format: '<turn_number>... <your_move>'."
+    "content": system_prompt_content
 }
 
 # Initialize game_log to track the history of moves
@@ -28,15 +33,18 @@ game_log = {
 # Initialize prompt which will be updated every turn
 prompt = [system_prompt, game_log]
 
-turn_number = 1
-
-while True:
+board = Board()
+playing = True
+while playing:
 
     # Get user input
     user_input = input("User: ")
+    board.update(user_input)
+    # board.print()
     
     # Update game_log with White move
-    game_log["content"] += f"{turn_number}. {user_input.strip()} "
+    # game_log["content"] += f"{turn_number}. {user_input.strip()} "
+    game_log["content"] = user_input
     
     # Submit prompt to model
     chat_completion = client.chat.completions.create(
@@ -46,24 +54,28 @@ while True:
     
     # Update game_log with Black move
     model_response_raw = chat_completion.choices[0].message.content
+    # print(model_response_raw)
+    board.update(model_response_raw)
+    board.print()
     
-    # Check if input is in correct format since model sometimes provides moves as '3... d5'
-    if '...' in model_response_raw: 
-        model_response = model_response_raw.split()[1]
-    else:
-        model_response = model_response_raw
+    # # Check if input is in correct format since model sometimes provides moves as '3... d5'
+    # if '...' in model_response_raw: 
+    #     model_response = model_response_raw.split()[1]
+    # else:
+    #     model_response = model_response_raw
     
-    game_log["content"] += f"{model_response} "
+    # game_log["content"] += f"{model_response} "
     
-    # for windows
-    if name == 'nt':
-        _ = system('cls')
-    # for mac and linux(here, os.name is 'posix')
-    else:
-        _ = system('clear')
+    # # for windows
+    # if name == 'nt':
+    #     _ = system('cls')
+    # # for mac and linux(here, os.name is 'posix')
+    # else:
+    #     _ = system('clear')
     
-    print(game_log["content"])
+    # print(game_log["content"])
     
-    turn_number += 1
+    board.fullmove_number += 1
+    playing = False
     
     

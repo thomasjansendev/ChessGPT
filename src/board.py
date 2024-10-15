16633b2d81dd5868b1a4411c21318d6586728cb5
thomasjansendev <thomasjansen.dev@gmail.com>
2024-10-15 13:40:03 +0200

    refactor(board-gui): (re)add assignement of rect attributes for Board squares and Piece classes
    *Board now initializes with Rects used by pygame to draw to the screen
    *Changed assignement of Piece.rect attribute -> no longer at initialization
    *Align changes to Piece.colour attribute -> simple string instead of an enum

--- src/board.py
+++ src/board.py
@@ -12,9 +12,10 @@ class Board:
         self.captured_pieces = { "w": [], "b": []}
         fill_board(self)
         # GUI attributes
-        self.sprites = {} #TODO: use this to store {'square': {'img': _}, {'rect': _} } to draw the GUI
+        self.sprites = init_board_sprites()
+        init_piece_sprites(self)
         # Games state attributes
-        self.active_colour = 'w'
+        self.active_colour = 'w' # or 'b'
         self.castling_availability = {
             "white_kingside": True,
             "white_queenside": True,
@@ -80,14 +81,13 @@ class Board:
     
     def get_piece(self,square: str) -> Piece:
         index = name_to_idx(square)
-        piece = self.array[index[0]][index[1]]
-        return piece
+        return self.array[index[0]][index[1]]
     
     def capture_piece(self,piece):
-        if piece.colour == colour.WHITE:
+        if piece.colour == 'w':
             self.active_pieces["w"].remove(piece)
             self.captured_pieces["w"].append(piece)
-        elif piece.colour == colour.BLACK:
+        elif piece.colour == 'b':
             self.active_pieces["b"].remove(piece)
             self.captured_pieces["b"].append(piece)
     
@@ -124,78 +124,85 @@ def verify_check():
 def verify_castling():
     return ''
 
-def init_empty_board():
+def init_empty_board() -> list:
     return [[None for _ in range(8)] for _ in range(8)]
 
-def fill_board(board):
+def fill_board(board: list) -> None:
     
     # WHITE PIECES
-    set_piece(Rook, colour.WHITE, "a1", board)
-    set_piece(Knight, colour.WHITE, "b1", board)
-    set_piece(Bishop, colour.WHITE, "c1", board)
-    set_piece(Queen, colour.WHITE, "d1", board)
-    set_piece(King, colour.WHITE, "e1", board)
-    set_piece(Bishop, colour.WHITE, "f1", board)
-    set_piece(Knight, colour.WHITE, "g1", board)
-    set_piece(Rook, colour.WHITE, "h1", board)
+    set_piece(Rook, 'w', "a1", board)
+    set_piece(Knight, 'w', "b1", board)
+    set_piece(Bishop, 'w', "c1", board)
+    set_piece(Queen, 'w', "d1", board)
+    set_piece(King, 'w', "e1", board)
+    set_piece(Bishop, 'w', "f1", board)
+    set_piece(Knight, 'w', "g1", board)
+    set_piece(Rook, 'w', "h1", board)
     
     # WHITE PAWNS
     white_pawn_squares = ["a2","b2","c2","d2","e2","f2","g2","h2"]
     for square in white_pawn_squares:
-        set_piece(Pawn, colour.WHITE, square, board)
+        set_piece(Pawn, 'w', square, board)
     
     # BLACK PIECES
-    set_piece(Rook, colour.BLACK, "a8", board)
-    set_piece(Knight, colour.BLACK, "b8", board)
-    set_piece(Bishop, colour.BLACK, "c8", board)
-    set_piece(Queen, colour.BLACK, "d8", board)
-    set_piece(King, colour.BLACK, "e8", board)
-    set_piece(Bishop, colour.BLACK, "f8", board)
-    set_piece(Knight, colour.BLACK, "g8", board)
-    set_piece(Rook, colour.BLACK, "h8", board)
+    set_piece(Rook, 'b', "a8", board)
+    set_piece(Knight, 'b', "b8", board)
+    set_piece(Bishop, 'b', "c8", board)
+    set_piece(Queen, 'b', "d8", board)
+    set_piece(King, 'b', "e8", board)
+    set_piece(Bishop, 'b', "f8", board)
+    set_piece(Knight, 'b', "g8", board)
+    set_piece(Rook, 'b', "h8", board)
     
     # BLACK PAWNS
     black_pawn_squares = ["a7","b7","c7","d7","e7","f7","g7","h7"]
     for square in black_pawn_squares:
-        set_piece(Pawn, colour.BLACK, square, board)
+        set_piece(Pawn, 'b', square, board)
 
-def set_piece(piece_type: Piece, colour: colour, square: str, board: Board):
+def set_piece(piece_type: Piece, colour: str, square: str, board: Board) -> None:
     # Initialize new piece
     new_piece = piece_type(colour)
     # Update board with new piece at specified square
     index_loc = name_to_idx(square)
     board.array[index_loc[0]][index_loc[1]] = new_piece
-    # Update piece dictionary with new piece
-    if colour == colour.WHITE:
+    # Update active piece dictionary with new piece
+    if colour == 'w':
         board.active_pieces["w"].append(new_piece)
-    elif colour == colour.BLACK:
+    elif colour == 'b':
         board.active_pieces["b"].append(new_piece)
+
+def init_board_sprites() -> dict:
+    # Creates a dictionary used by pygame to draw to screen:
+    #   - key = square on the board 
+    #   - value = data needed for pygame to draw a GUI (image, rect)
     
-# TODO: refactor to create self.sprites dictionary instead {'square': {'img': _}, {'rect': _} } 
-def init_board_dict(): 
-    # Returns a dictionary containing a key for each square on the board
-    # Initiliazes an empty board -> set_pieces() is used to fill board with starting piece positions 
-    board_dict = dict()
+    gui_dict = {}
     
-    is_light = False
+    is_light = False # simple bool to alternate between light/dark squares
     for row in range(0,len(BOARD_REF)):
         is_light = not is_light
         for col in range(0,len(BOARD_REF[row])):
             square_name = BOARD_REF[row][col]
-            if is_light:
-                square_img = SPRITES_DICT["square_light"]
-            else:
-                square_img = SPRITES_DICT["square_dark"]
+            square_img = SPRITES_DICT["square_light"] if is_light else SPRITES_DICT["square_dark"]
             
             x = col * CELL_WIDTH
             y = row * CELL_HEIGHT
-            square_img_rect = square_img.get_rect(topleft=(x,y))
+            square_rect = square_img.get_rect(topleft=(x,y))
             
-            board_dict[square_name] = {'index': (row,col), # Used to convert to array to get_possible_moves
-                                       'img': square_img, # Used to store sprite to draw to screen
-                                       'rect': square_img_rect, # Used to store the square's hitbox
-                                       'piece': None # used to store the current piece on the square
-                                       }
+            gui_dict[square_name] = {
+                'img': square_img, # Used to store sprite to draw to screen
+                'rect': square_rect, # Used to store the square's hitbox
+                }
             is_light = not is_light
-            
-    return board_dict
+    
+    return gui_dict
+
+def init_piece_sprites(board: Board):
+    # Initializes the rect of pieces to their corresponding square
+    # Inneficient because this could be done when calling fill_board() and set_piece()
+    # but at least the backend and frontend operations are decoupled and I can more easily swap to a different frontend when needed 
+    # + it is an operation done a initialization and not at runtime
+    for piece in board.active_pieces["w"] + board.active_pieces["b"]:
+        square = piece.get_position(board.array)
+        square_rect = board.sprites[square]["rect"]
+        piece.rect = piece.img.get_rect(topleft=(square_rect.x, square_rect.y))
